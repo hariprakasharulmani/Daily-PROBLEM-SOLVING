1e4e1a0119356db7666d1675684d8ce9e003a6df
hariprakasharulmani <hariprakasharulmani28@gmail.com>
2026-08-02 15:03:05 +0530

    Add program to print array elements in reverse order

---
+++ Print array elements in reverse order.java
@@ -0,0 +1,11 @@
+
+//Print array elements in reverse order.
+
+class Main {
+    public static void main(String[] args) {
+         int[] arr = {1, 4, 3, 2, 6, 5};
+         for(int i=arr.length-1;i>=0;i--){
+             System.out.print(arr[i]+" ");
+         }
+    }
+}
