ffa4eb2dc15e3bf9c8815049a139db738a8df70d
hariprakasharulmani <hariprakasharulmani28@gmail.com>
2026-08-24 15:43:20 +0530

    Create 3452. Sum of Good Numbers.java

---
+++ 3452. Sum of Good Numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            if( (i-k >= 0) && (i+k < nums.length) ){
+                if( (nums[i-k] < nums[i] ) && (nums[i+k] < nums[i]) ) sum+=nums[i];
+            }
+            else if( (i-k >= 0) || (i+k < nums.length) ){
+                if(i-k >= 0){
+                    if(nums[i]>nums[i-k]) sum+=nums[i];
+                }
+                if(i+k < nums.length){
+                    if(nums[i]>nums[i+k]) sum+=nums[i];
+                } 
+                //sum+=nums[i];
+            }
+            else{
+                sum+=nums[i];
+            } 
+        }
+        return sum;
+    }
+}
